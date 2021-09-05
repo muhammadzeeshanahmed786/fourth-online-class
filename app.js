f44bf6cba35a9d68e77387196c71f066515d8360
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-04 23:35:59 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,299 @@
+
+
+
+
+
+// function breakFast(){
+//     console.log("bring me a break fast");
+// }
+
+
+// breakFast();
+
+
+
+
+// function cookFood(dishName){
+//     console.log("Bring me : " , dishName)
+// }
+
+// cookFood("Tea")
+// cookFood("biscuits")
+// cookFood("biryani")
+// cookFood("qorma")
+// cookFood("chpati")
+
+
+
+
+
+// function cookFood(dishName,serving){
+//         console.log("Bring me : " , dishName + " for " + serving + " person")
+//     }
+
+//     cookFood("chicken biryani", 3)
+    // cookFood("",3 )
+
+
+
+    // function sum(first,scond){
+    //     console.log("Adnan Get Marks " , first + " Out of" , scond)
+    // }
+
+    // sum(40 , 100)
+
+
+
+
+
+// function fullName(first,second){
+//     console.log("Well Come " + first  +" " + second)
+// }
+
+
+// var userNam1=prompt("Enter a First Name");
+// var userNam2=prompt("Enter a Last Name");
+
+// fullName(userNam1 , userNam2 )
+
+
+
+
+
+// function sum2Numbers(sum1, sum2){
+//     var sum = sum1+sum2;
+//     return sum
+// }
+
+// var sum=sum2Numbers(2,4)
+
+// console.log(sum)
+
+
+// function printUserNae(){
+//     var user="zeeshan"
+
+//     // return user
+
+// }
+
+// var userName=printUserNae();
+
+// console.log(userName)
+
+
+
+
+
+
+
+
+
+
+
+
+// function userName(first,last){
+//     var first =prompt("Enter Name");
+//     var last =prompt("Enter Name");
+//     sum=first+" "+last;
+//     return sum
+// }
+
+// var name=userName();
+// console.log(name)
+
+
+
+
+
+// function sum(first,second){
+
+//     // var sum=first+second
+//     return first + second;
+// }
+
+// var sum1=sum(10,40)
+// console.log(sum1)
+
+
+
+
+// function sub(first,second){
+
+//     // var sum=first+second
+//     return first -  second;
+// }
+
+// var sum1=sub(40,10)
+// console.log(sum1)
+
+
+// function square(){
+//     var pro=prompt("Enter a Number")
+
+//     return pro * pro
+// }
+
+// var sq=square()
+
+// console.log(sq)
+
+
+
+
+
+
+
+
+
+
+
+// function square(){
+//         var pro=prompt("Enter a Number")
+//         return pro*pro
+//     }
+
+
+// function formula(num1,num2){
+
+//         var firstSquare=square(num1)
+//         var secondSquare=square(num2)
+//         var equation=num1*num1 + 
+
+// }
+
+
+
+
+
+// function square(num){
+//     return num*num
+// }
+
+// function formula(num1,num2){
+//     var firstSquare=square(num1);
+//     var secondSquare=square(num2);
+//     var equation= firstSquare + 2* num1*num2 +secondSquare
+//     return equation
+// }
+
+
+
+// var calculatedValue=formula(2,3)
+// console.log(calculatedValue)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function square(num){
+//     return num*num
+// }
+
+// function formula(num1,num2){
+//     var First=square(num1)
+//     var second=square(num2)
+
+//     var sum=First+second;
+//     return sum
+// }
+// var cal=formula(2,3)
+// console.log(cal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
